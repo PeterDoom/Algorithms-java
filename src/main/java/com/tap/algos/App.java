d57c415231e0212abb617511ca5dd0601557944f
Plamen Nanev <48176167+PeterDoom@users.noreply.github.com>
2021-11-26 22:03:10 +0200

    Update App.java

--- src/main/java/com/tap/algos/App.java
+++ src/main/java/com/tap/algos/App.java
@@ -2,8 +2,6 @@ package com.tap.algos;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws IOException {
