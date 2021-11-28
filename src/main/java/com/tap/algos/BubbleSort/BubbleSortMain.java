333eb1940dbced97e788becaf76dcad69b0b480a
PeterDoom <48176167+PeterDoom@users.noreply.github.com>
2021-11-28 19:56:26 +0200

    Implementin Buble Sort

--- src/main/java/com/tap/algos/BubbleSort/BubbleSortMain.java
+++ src/main/java/com/tap/algos/BubbleSort/BubbleSortMain.java
@@ -1,6 +1,37 @@
 package com.tap.algos.BubbleSort;
 
+import com.tap.algos.Utils;
+
+import java.util.Arrays;
+
 public class BubbleSortMain {
 
+    public static int[] sort(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+
+                int currValue = arr[j];
+                int nextValue = arr[j + 1];
+
+                if (currValue > nextValue) {
+                    arr[j] = nextValue;
+                    arr[j + 1] = currValue;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static String[] sort(String[] arr) {
+        return arr;
+    }
+
+
+    public static void main(String[] args) {
+        int[] testArr = Utils.generateArray(5);
+        System.out.println("Sorted: " + Arrays.toString(BubbleSortMain.sort(testArr)));
+    }
 
 }
