3624ed9e79ad18250b2b5f399f66ccf5164e1578
PeterDoom <48176167+PeterDoom@users.noreply.github.com>
2021-11-28 20:03:51 +0200

    Bubble Sort with String works now too

--- src/main/java/com/tap/algos/BubbleSort/BubbleSortMain.java
+++ src/main/java/com/tap/algos/BubbleSort/BubbleSortMain.java
@@ -25,6 +25,18 @@ public class BubbleSortMain {
     }
 
     public static String[] sort(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+
+                String currValue = arr[j];
+                String nextValue = arr[j + 1];
+
+                if (currValue.compareTo(nextValue) > 0) {
+                    arr[j] = nextValue;
+                    arr[j + 1] = currValue;
+                }
+            }
+        }
         return arr;
     }
 
@@ -32,6 +44,8 @@ public class BubbleSortMain {
     public static void main(String[] args) {
         int[] testArr = Utils.generateArray(5);
         System.out.println("Sorted: " + Arrays.toString(BubbleSortMain.sort(testArr)));
+        String[] testStringArr = Utils.generateStringArray(5);
+        System.out.println("Sorted: " + Arrays.toString(BubbleSortMain.sort(testStringArr)));
     }
 
 }
