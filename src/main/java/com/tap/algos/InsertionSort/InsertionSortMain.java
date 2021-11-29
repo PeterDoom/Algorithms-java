d3f65b86ff125da9a2fa63bdd66e0ce3e8429f34
PeterDoom <48176167+PeterDoom@users.noreply.github.com>
2021-11-29 20:53:55 +0200

    Starting the Insertion Sort Algo

--- src/main/java/com/tap/algos/InsertionSort/InsertionSortMain.java
+++ src/main/java/com/tap/algos/InsertionSort/InsertionSortMain.java
@@ -2,4 +2,11 @@
 package com.tap.algos.InsertionSort;
 
 public class InsertionSortMain {
+    public static int[] sort(int[] arr) {
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
