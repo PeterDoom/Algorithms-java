c2d21f900bda15559bda6035aa1f5dec6cd49847
PeterDoom <48176167+PeterDoom@users.noreply.github.com>
2021-12-06 20:09:51 +0200

    Adding an unshift function

--- src/main/java/com/tap/algos/InsertionSort/InsertionSortMain.java
+++ src/main/java/com/tap/algos/InsertionSort/InsertionSortMain.java
@@ -3,7 +3,15 @@ package com.tap.algos.InsertionSort;
 
 public class InsertionSortMain {
     public static int[] sort(int[] arr) {
+
+
         return arr;
+    }
+
+    private int[] unshift(int[] arrToUnshift, int value) {
+
+
+        return arrToUnshift;
     }
 
     public static void main(String[] args) {
